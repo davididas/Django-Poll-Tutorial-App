a608845ce350ff552f00187ffef4b6192ace0490
David Quiroz <davidaquiroz@gmail.com>
2014-03-13 15:08:49 -0700

    switched to Generic Views

--- polls/views.py
+++ polls/views.py
@@ -1,22 +1,28 @@
 from django.shortcuts import render, get_object_or_404
-from django.http import HttpResponseRedirect, HttpResponse
+from django.http import HttpResponseRedirect
 from django.core.urlresolvers import reverse
+from django.views import generic
 from polls.models import Poll, Choice
 
 # Create your views here.
 
-def index(request):
-  latest_poll_list = Poll.objects.order_by('-pub_date')[:5]
-  context = {'latest_poll_list': latest_poll_list}
-  return render(request, 'polls/index.html', context)
+class IndexView(generic.ListView):
+    template_name = 'polls/index.html'
+    context_object_name = 'latest_poll_list'
 
-def detail(request, poll_id):
-  poll = get_object_or_404(Poll, pk=poll_id)
-  return render(request, 'polls/detail.html', {'poll':poll})
+    def get_queryset(self):
+        """Return the last five published polls."""
+        return Poll.objects.order_by('-pub_date')[:5]
 
-def results(request, poll_id):
-  poll = get_object_or_404(Poll, pk=poll_id)
-  return render(request, 'polls/results.html', {'poll':poll})
+
+class DetailView(generic.DetailView):
+    model = Poll
+    template_name = 'polls/detail.html'
+
+
+class ResultsView(generic.DetailView):
+    model = Poll
+    template_name = 'polls/results.html'
 
 def vote(request, poll_id):
   p = get_object_or_404(Poll, pk=poll_id)
