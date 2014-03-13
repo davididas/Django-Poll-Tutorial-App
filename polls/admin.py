f5959148106f50935221cea3b1a26c6a1c9182a3
David Quiroz <davidaquiroz@gmail.com>
2014-03-13 12:02:14 -0700

    init  - basic poll funcitonality

---
+++ polls/admin.py
@@ -0,0 +1,24 @@
+from django.contrib import admin
+from polls.models import Choice, Poll
+# Register your models here.
+
+
+class ChoiceInline(admin.TabularInline):
+	model = Choice
+	extra = 3
+
+
+class PollAdmin(admin.ModelAdmin):
+	list_display = ('question', 'pub_date', 'was_published_recently')
+	fieldsets = [
+		(None, 								{'fields': ['question']}),
+		('Date Information', 	{'fields': ['pub_date'], 'classes': ['collapse']}),
+	]
+	inlines = [ChoiceInline]
+	list_filter = ['pub_date']
+	search_fields = ['question']
+
+
+
+admin.site.register(Poll, PollAdmin)
+admin.site.register(Choice)
