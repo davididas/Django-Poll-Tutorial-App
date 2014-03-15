b280b6946c79179c007ddacc481de3df5f824d38
David Quiroz <davidaquiroz@gmail.com>
2014-03-14 19:07:49 -0700

    Various - addd Bootstrap styling - added some tests - Views: ignore scheduled polls on index

--- polls/views.py
+++ polls/views.py
@@ -2,6 +2,7 @@ from django.shortcuts import render, get_object_or_404
 from django.http import HttpResponseRedirect
 from django.core.urlresolvers import reverse
 from django.views import generic
+from django.utils import timezone
 from polls.models import Poll, Choice
 
 # Create your views here.
@@ -11,8 +12,13 @@ class IndexView(generic.ListView):
     context_object_name = 'latest_poll_list'
 
     def get_queryset(self):
-        """Return the last five published polls."""
-        return Poll.objects.order_by('-pub_date')[:5]
+      """
+      Return the last five published polls (not including those set to be
+      published in the future).
+      """
+      return Poll.objects.filter(
+          pub_date__lte=timezone.now()
+      ).order_by('-pub_date')[:5]
 
 
 class DetailView(generic.DetailView):
